31dbc6a432981d259c6f16e9ffde2e760cee2c09
dengxic <435700611@qq.com>
2016-08-22 18:18:04 +0800

    拿回家

---
+++ app/src/main/java/com/king/luhai/Activity/HomepageActivity.java
@@ -0,0 +1,18 @@
+package com.king.luhai.Activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+import com.king.luhai.R;
+
+/**
+ * Created by Administrator on 2016/8/22.
+ */
+public class HomepageActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
