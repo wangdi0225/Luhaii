86b719dfba79d70a1641b8ba1becfc7fba249ca3
ly920525 <710201459@qq.com>
2016-08-24 14:09:22 +0800

    2

---
+++ app/src/main/java/com/king/luhai/Activity/FaXiaoXi.java
@@ -0,0 +1,18 @@
+package com.king.luhai.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.king.luhai.R;
+
+/**
+ * Created by Administrator on 2016/8/24 0024.
+ */
+public class FaXiaoXi extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.faxiaoxi);
+
+    }
+}
---
+++ app/src/main/java/com/king/luhai/Activity/ZHGuanLi.java
@@ -0,0 +1,17 @@
+package com.king.luhai.Activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.king.luhai.R;
+
+/**
+ * Created by Administrator on 2016/8/24 0024.
+ */
+public class ZHGuanLi extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.zhguanli);
+    }
+}
