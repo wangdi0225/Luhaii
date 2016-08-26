1ad2b9d0d92af53ea5a12e19299ad28f02ceb4c6
ly920525 <710201459@qq.com>
2016-08-26 08:58:23 +0800

    123

--- app/src/main/java/com/king/luhai/Activity/MyData.java
+++ app/src/main/java/com/king/luhai/Activity/MyData.java
@@ -1,9 +1,14 @@
 package com.king.luhai.Activity;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.king.luhai.R;
 
@@ -11,6 +16,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.Inflater;
 
 /**
  * Created by Administrator on 2016/8/22 0022.
@@ -18,34 +24,43 @@ import java.util.Map;
 public class MyData extends Activity {
     ListView data_lv;
     SimpleAdapter sa;
-    List<Map<String,Object>> list=new ArrayList<Map<String, Object>>();
+
+    List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mydata);
         chushi();
         data_lv.setAdapter(sa);
+
     }
-    private void chushi(){
-        data_lv= (ListView) findViewById(R.id.data_lv);
-        String[] from={"tx","ms","jg","js","sw","zg","rq","time"};
-        int[] to={R.id.data_item_tx,R.id.data_item_ms,R.id.data_item_jg,R.id.data_item_js,R.id.data_item_sw,R.id.data_item_zg,R.id.data_item_rq,R.id.data_item_time,};
-        sa=new SimpleAdapter(this,getList(),R.layout.data_item,from,to);
-        }
+
+    private void chushi() {
+        data_lv = (ListView) findViewById(R.id.data_lv);
+
+        String[] from = {"tx", "ms", "jg", "js", "sw", "zg", "rq", "time"};
+        int[] to = {R.id.data_item_tx, R.id.data_item_ms, R.id.data_item_jg, R.id.data_item_js, R.id.data_item_sw, R.id.data_item_zg, R.id.data_item_rq, R.id.data_item_time,};
+        sa = new SimpleAdapter(this, getList(), R.layout.data_item, from, to);
+
+    }
 
     public List<Map<String, Object>> getList() {
-        for (int i = 0; i <20 ; i++) {
-            Map<String,Object> map=new HashMap<String, Object>();
-            map.put("tx",R.mipmap.ic_launcher);
-            map.put("ms","经典匹配");
-            map.put("jg","胜利");
-            map.put("js","14");
-            map.put("sw","1");
-            map.put("zg","12");
-            map.put("rq","2月22日");
-            map.put("time","14.22");
+        for (int i = 0; i < 20; i++) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("tx", R.mipmap.ic_launcher);
+            map.put("ms", "经典匹配");
+            map.put("jg", "失败");
+            map.put("js", "14");
+            map.put("sw", "1");
+            map.put("zg", "12");
+            map.put("rq", "2月22日");
+            map.put("time", "14.22");
             list.add(map);
         }
         return list;
     }
+
+
 }
+
