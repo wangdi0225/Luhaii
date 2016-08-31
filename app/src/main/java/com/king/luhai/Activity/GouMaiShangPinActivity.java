4cf4605455259c0773ff21bbf2367dee4bb5469d
dengxic <435700611@qq.com>
2016-08-31 14:44:57 +0800

    修改冲突

--- app/src/main/java/com/king/luhai/Activity/GouMaiShangPinActivity.java
+++ app/src/main/java/com/king/luhai/Activity/GouMaiShangPinActivity.java
@@ -40,12 +40,8 @@ public class GouMaiShangPinActivity extends Activity {
     public List<Map<String, Object>> getLists() {
 
         Map<String, Object> map = new HashMap<String, Object>();
-<<<<<<< HEAD
 
         map.put("images", R.mipmap.timod);
-=======
-        map.put("images", R.mipmap.timo2);
->>>>>>> hutingrui
         map.put("mingzi", "迅捷斥候");
         map.put("jiage", "500");
         lists.add(map);
--- app/src/main/java/com/king/luhai/Activity/ShouShangPinActivity.java
+++ app/src/main/java/com/king/luhai/Activity/ShouShangPinActivity.java
@@ -40,13 +40,10 @@ public class ShouShangPinActivity extends Activity {
     public List<Map<String, Object>> getLists() {
 
         Map<String, Object> map = new HashMap<String, Object>();
-<<<<<<< HEAD
+
 
         map.put("images", R.mipmap.timod);
 
-=======
-        map.put("images", R.mipmap.timo2);
->>>>>>> hutingrui
         map.put("mingzi","迅捷斥候");
         map.put("jiage","500");
         lists.add(map);
